9bf0aad81831f3319edba1d19ae199c093bd7104
hllqkb <androidhtml@yandex.com>
2024-08-27 14:44:10 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'), // 压缩js文件
-    sass = require('gulp-sass')(require('sass')), // 编译sass
+    sass = require('gulp-sass'), // 编译sass
     cleanCSS = require('gulp-clean-css'), // 压缩css文件
     rename = require('gulp-rename'); // 文件重命名
 
-gulp.task('scripts', function(done){
+gulp.task('scripts', function(){
     gulp.src('dev/js/index.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('assets/js'));
-    done();
+        .pipe(gulp.dest('assets/js'))
 });
 
-gulp.task('sass', function(done){
+gulp.task('sass', function(){
     gulp.src('dev/sass/app.scss')
         .pipe(sass())
         .pipe(gulp.dest('dev/sass'))
         .pipe(cleanCSS())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('assets/css'));
-    done();
 });
 
+gulp.task('watch', function(){
+    gulp.watch('dev/sass/*.scss', ['sass']);
+    gulp.watch('dev/js/*.js', ['scripts']);
+});
 
-gulp.task('default', gulp.series('scripts', 'sass'));
+gulp.task('default', ['scripts', 'sass', 'watch']);
